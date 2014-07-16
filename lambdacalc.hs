c64f43753b930a128c7783c5c2cefc6133b821c6
Garrison Jensen <garrison.jensen@gmail.com>
2014-07-16 14:42:01 -0700

    Simple parser for lambda calculus.

---
+++ lambdacalc.hs
@@ -0,0 +1,64 @@
+-- Copyright © 2014 Garrison Jensen
+-- License
+-- This code and text are dedicated to the public domain. 
+-- You can copy, modify, distribute and perform the work, 
+-- even for commercial purposes, all without asking permission.
+-- For more information, please refer to <http://unlicense.org/> 
+-- or the accompanying LICENSE file. 
+
+import Text.Parsec
+import Text.Parsec.String
+import Data.Either
+
+type VarName = Char 
+
+data Expr
+  = Var VarName
+  | Lam VarName Expr
+  | App Expr Expr
+  deriving (Eq, Show)
+
+lam :: Parser Expr
+lam = do
+  char '\\'
+  n <- letter
+  string "."
+  e <- expr
+  return $ Lam n e
+
+app :: Parser Expr
+app = do
+  apps <- many1 term
+  return $ foldl1 App apps
+
+var :: Parser Expr
+var = do
+  n <- letter
+  return $ Var n
+
+parens :: Parser Expr -> Parser Expr
+parens p = do
+  char '('
+  e <- p
+  char ')'
+  return e
+
+term :: Parser Expr
+term = var <|> parens expr
+
+expr :: Parser Expr
+expr = lam <|> app
+
+decl :: Parser Expr
+decl = do
+  e <- expr
+  eof
+  return e
+
+test :: IO ()
+test = do 
+  n <- getLine 
+  parseTest decl n
+
+main :: IO ()
+main = test >>= print >> main
